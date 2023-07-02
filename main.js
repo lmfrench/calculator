3dc8409d7d4fcc68a8623121b2215ad68b2f06c6
lmfrench <lisa_french@hotmail.com>
2023-07-02 21:37:24 +0100

    Add operate function

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ function multiply (num1, num2){
 function divide (num1, num2){
     return num1 / num2;
 }
+
+function operate (num1, num2, operator){
+    
+    switch (operator){
+        
+        case '+': return add(num1, num2);
+        break;
+        case '-': return subtract(num1, num2);
+        break;
+        case 'x': return multiply(num1, num2);
+        break;
+        case '/': return divide(num1, num2);      
+        
+    }
+}
+
+console.log(operate(1, 2, '+'));
+console.log(operate(1, 2, '-'));
+console.log(operate(1, 2, 'x'));
+console.log(operate(1, 2, '/'));

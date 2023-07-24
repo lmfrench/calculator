eb2eb814684a36d84508e1ca2764a63dc6cfee95
lmfrench <lisa_french@hotmail.com>
2023-07-24 18:06:35 +0100

    Add keyboard functionality

--- main.js
+++ main.js
@@ -47,6 +47,8 @@ function operate (num1, num2, operator){
         break;
         case 'x': return multiply(num1, num2);
         break;
+        case '*': return multiply(num1, num2);
+        break;
         case '/': return divide(num1, num2);      
         
     }
@@ -68,14 +70,13 @@ function checkDecimal (num){
     }
 }
 
-
-//Event listener on calculator numbers
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('#result')
 let num1 = "";
 let num2 = "";
 let operator ="";
 
+//Code to allow user to do calculations by pressing buttons
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         let type = button.getAttribute('id');
@@ -84,7 +85,7 @@ buttons.forEach((button) => {
             //Displays operator entered
             result.textContent = button.textContent;
             
-            //if this isn't the first calculation (i.e both num variables have values), then do the next calculation
+            //checks whether this is the first calculation. If this isn't the first calculation (i.e both num variables have values), then do a calculation
             if (num2 !== ""){
                 num1 = operate(num1, num2, operator);
                 
@@ -92,7 +93,7 @@ buttons.forEach((button) => {
                 // then clear num 2
                 num2 = "";
             }            
-            //stores operator if first calculation
+            //stores operator if this is the first calculation
             operator = button.textContent;         
         }
 
@@ -100,7 +101,7 @@ buttons.forEach((button) => {
            //calculates result if two numbers have been entered
            if (num1 !== "" && num2 !== ""){
             result.textContent = operate(num1, num2, operator);
-            //stores result and clears num2
+            //stores result in num1 and clears num2 so another calulation could be performed on the result
             num1 = operate(num1, num2, operator);
             num2 = "";              
             }
@@ -112,7 +113,7 @@ buttons.forEach((button) => {
 
        // delete last number if delete pressed
         else if (type === 'del') {
-            //check whether on num1 or num2
+            //check whether to delete a character from num1 or num2
             if (num2 === ""){
                 num1 = num1.slice(0,-1);
                 result.textContent = num1;
@@ -123,8 +124,7 @@ buttons.forEach((button) => {
             }
         }
 
-        // clear everything if Clear pressed
-        
+        // clear everything if Clear button pressed
         else if (type === 'cl') {     
             num1="";
             num2="";
@@ -143,8 +143,9 @@ buttons.forEach((button) => {
                     // stores number
                     else {
                         num1 += button.textContent;
-                  //displays numbers entered                    
+                              
                     }
+                    //displays numbers entered     
                     result.textContent = num1;
                 }
             }   
@@ -153,13 +154,13 @@ buttons.forEach((button) => {
             else{
                 //check decimal is not being selected for second time
                 if (type === 'decimal'){
-                    if(num2.length < 8){
+                    if(num2.length < 8){ //prevent number being larger than result screen
                         num2 = checkDecimal(num2);
                         result.textContent = num2;
                     }
                 }
                 else{
-                    if(num2.length < 8){
+                    if(num2.length < 8){ //prevent number being larger than result screen
                         // stores number
                         num2 += button.textContent;
                     }
@@ -171,3 +172,76 @@ buttons.forEach((button) => {
     });
 });
 
+//Code to allow user to do calculations by keyboard input
+
+document.addEventListener('keydown', (event) => {
+    var keyPressed = event.key;
+    
+    
+    if (keyPressed === '/' || keyPressed === '+' || keyPressed === '-' || keyPressed === '*'){
+        result.textContent = keyPressed;
+            //checks whether this is the first calculation. If this isn't the first calculation (i.e both num variables have values), then do a calculation
+            if (num2 !== ""){
+                num1 = operate(num1, num2, operator);
+                result.textContent = num1;
+                // then clear num 2
+                num2 = "";
+            }            
+            //stores operator if this is the first calculation
+            operator = keyPressed;  
+    }
+
+    else if (keyPressed === '=' || keyPressed === 'Enter') {        
+        console.log(num1, num2);    
+        //calculates result if two numbers have been entered
+        if (num1 !== "" && num2 !== ""){
+         result.textContent = operate(num1, num2, operator);
+         //stores result in num1 and clears num2 so another calulation could be performed on the result
+         console.log(operate(num1, num2, operator));
+         num1 = operate(num1, num2, operator);
+         num2 = "";      
+                
+         }
+         //or if only num1 entered, continue to show num1
+         else if (num1 !== ""){
+             result.textContent = num1;
+         }
+     }
+
+    if (Number(keyPressed) < 10 || keyPressed === '.' ){
+        // store num1 if an operator HASN'T been selected
+        if (operator === ""){
+            //check decimal is not being selected for second time
+            if(num1.length < 8){ //prevent number being larger than result screen
+                if (keyPressed === '.'){
+                    num1 = checkDecimal(num1);              
+                }
+                // stores number
+                else {
+                    num1 += keyPressed;                       
+                }
+                //displays numbers entered     
+                result.textContent = num1;
+            }
+        }   
+
+        // store num2 if an operator HAS been selected
+        else{
+            //check decimal is not being selected for second time
+            if (keyPressed === '.'){
+                if(num2.length < 8){ //prevent number being larger than result screen
+                    num2 = checkDecimal(num2);
+                    result.textContent = num2;
+                }
+            }
+            else{
+                if(num2.length < 8){ //prevent number being larger than result screen
+                // stores number
+                num2 += keyPressed;
+                }
+            }
+            //displays numbers entered
+            result.textContent = num2;
+        }
+    }   
+});
